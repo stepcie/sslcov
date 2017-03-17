184805858cbe73aa47856d7e9fd8aaafb85ce952
Boris <boris.hejblum.ensai@gmail.com>
2017-03-17 18:07:49 +0100

    Documenting expit and logit

--- R/logit.R
+++ R/logit.R
@@ -1,5 +1,11 @@
 #'expit
 #'
+#'expit function
+#'
+#'@param x a real number
+#'
+#'@return a real number on the probabilty scale (between \code{0} and \code{1})
+#'
 #'@export
 expit <- function(x){
   return(exp(x)/(1+exp(x)))
@@ -7,6 +13,12 @@ expit <- function(x){
 
 
 #'logit
+#'
+#'logit function
+#'
+#'@param p a number between \code{0} and \code{1} (e.g. a probability)
+#'
+#'@return a real number between \code{-Inf} and \code{Inf}
 #'
 #'@export
 logit <- function(p){
